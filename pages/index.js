f7c7678a94e9b455c0ac9cd6bb9deda01fb4f733
Marin Umegane <marin.umegane.m@gmail.com>
2026-06-07 15:18:28 -0400

    fix responsive design

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-import "./index.css";
+const toggle = document.querySelector('.nav__toggle');
+const links = document.querySelector('.nav__links');
+
+toggle.addEventListener('click', () => {
+  const isOpen = links.classList.toggle('nav__links--open');
+  toggle.setAttribute('aria-expanded', isOpen);
+});
+
+// close menu when a link is clicked
+document.querySelectorAll('.nav__link').forEach(link => {
+  link.addEventListener('click', () => {
+    links.classList.remove('nav__links--open');
+    toggle.setAttribute('aria-expanded', 'false');
+  });
+});
